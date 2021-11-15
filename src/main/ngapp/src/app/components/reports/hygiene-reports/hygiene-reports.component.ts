519ba42f3389df9229f7a4ca477f7639dbba8cc1
Hector Plahar <hector.plahar@gmail.com>
2021-11-15 08:33:15 -0800

    hand hygiene reports component

--- src/main/ngapp/src/app/components/reports/hygiene-reports/hygiene-reports.component.ts
+++ src/main/ngapp/src/app/components/reports/hygiene-reports/hygiene-reports.component.ts
@@ -14,15 +14,132 @@ export class HygieneReportsComponent implements OnInit {
     paging: Paging;
     reports: HygieneReport[];
 
+    multi: any[];
+
+    // options
+    showXAxis: boolean = true;
+    showYAxis: boolean = true;
+    gradient: boolean = false;
+    showLegend: boolean = false;
+    showXAxisLabel: boolean = false;
+    showYAxisLabel: boolean = false;
+
+    animations: boolean = true;
+
+    colorScheme = {
+        domain: ['#b10c0c', '#047c26', '#AAAAAA']
+    };
+
     constructor(private http: HttpService) {
         this.paging = new Paging();
+        // this.multi = [
+        //     {
+        //         "name": "13 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 6
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 13
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "name": "12 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 0
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 14
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "name": "11 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 19
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 6
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "name": "10 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 78
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 5
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "name": "9 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 98
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 100
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "name": "8 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 0
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 0
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         "name": "7 Nov",
+        //         "series": [
+        //             {
+        //                 "name": "Non-Compliance",
+        //                 "value": 3
+        //             },
+        //             {
+        //                 "name": "Compliance",
+        //                 "value": 74
+        //             }
+        //         ]
+        //     },
+        // ];
     }
 
     ngOnInit(): void {
         this.http.get('reports/hygiene/HAND', this.paging).subscribe((result: Result<HygieneReport>) => {
             this.paging.available = result.available;
             this.reports = result.requested;
-            console.log(result);
         })
+
+        // get hygiene graph data
+        this.http.get('reports/hygiene/HAND/graph').subscribe((result: any[]) => {
+            this.multi = result;
+        });
+    }
+
+    onSelect(event): void {
+        console.log(event);
     }
 }

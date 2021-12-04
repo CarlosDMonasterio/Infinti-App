53b1238c4baf1f0523c64a015f341e80d05c17ba
Carlos Diego Monasterio <carlosdiego.j.monasterio.ctr@mail.mil>
2021-12-03 20:03:46 -0800

    **Accounts.java: Wrapped resetPassword to bypass function if accountModel returns null.
    **Fixed forgot password page to patch login page css.  Form displays required fields error on null and display error <div> on failed password reset attempts.  Not able to confirm successful password resets yet.
    
    **http.services.ts: Change url redirect to 'False' to stop page from redirecting to login on error.
    
    **Fixed the link in the login page to navigate to the password reset page.
    
    **Fixed "Cancel" button link on the Register page.

--- src/main/ngapp/src/app/components/forgot-password/forgot-password.component.ts
+++ src/main/ngapp/src/app/components/forgot-password/forgot-password.component.ts
@@ -10,6 +10,7 @@ import {Router} from '@angular/router';
 export class ForgotPasswordComponent implements OnInit {
 
     userId = undefined;
+    idFieldEmpty = false;
     processing = false;
     passwordWasReset = false;
     errorResetting = false;
@@ -26,20 +27,31 @@ export class ForgotPasswordComponent implements OnInit {
         this.errorResetting = false;
 
         if (!this.userId) {
-            // Util.setError('Please enter your userId and try again');
+            this.idFieldEmpty = true;
             return;
         }
 
         this.processing = true;
-        this.http.post('users/' + this.userId + '/password', null).subscribe(response => {
-            this.processing = false;
-            this.passwordWasReset = true;
-        }, error => {
-            console.log(error);
-            this.processing = false;
-            this.errorResetting = true;
-        });
+        this.http
+            .post('users/' + this.userId + '/password', null)
+                .subscribe(response => {
+                    this.processing = false;
+                    this.passwordWasReset = true;
+
+                }, error => {
+                    console.log(error);
+                    this.processing = false;
+                    this.errorResetting = true;
+                });
     }
+
+    clearField(): void{
+        //TODO make dynamic so the name of the field is passed as a parameter.
+        this.userId= null;
+        this.errorResetting = false;
+        this.idFieldEmpty = false
+    }
+
 
     goToLogin(): void {
         this.router.navigate((['/login']));
